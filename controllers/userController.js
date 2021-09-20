770333728afeef1fd079f1f0d142fce4dde29234
aissamihi <aissamihi@gmail.com>
2021-09-20 19:13:25 -0400

    implement signout method, clear cookie

--- controllers/userController.js
+++ controllers/userController.js
@@ -38,3 +38,10 @@ exports.signin = (req, res) => {
     }
   });
 };
+
+exports.signout = (req, res) => {
+  res.clearCookie("token");
+  res.json({
+    message: "User Signout"
+  });
+};
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const { greeting, signup, signin } = require("../controllers/userController");
+const {
+  greeting,
+  signup,
+  signin,
+  signout
+} = require("../controllers/userController");
 const { userSignUpValidator } = require("../middlewares/userValidator");
 router.get("/", greeting);
 
 router.post("/signup", userSignUpValidator, signup);
 router.post("/signin", signin);
+router.get("/signout", signout);
 
 module.exports = router;
